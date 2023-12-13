0358d8f6523f4b91ca349b992ead598445bc786c
kykileur <kyliannfernandes@gmail.com>
2023-12-13 16:55:32 +0100

    la page process de la bdd

---
+++ src/Entity/Process.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProcessRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProcessRepository::class)]
+class Process
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $process = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $statut = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProcess(): ?string
+    {
+        return $this->process;
+    }
+
+    public function setProcess(string $process): static
+    {
+        $this->process = $process;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
