89f61e21e87af78244b766b28df3a7ea22712c75
kykileur <kyliannfernandes@gmail.com>
2023-12-14 10:21:36 +0100

    modif page process

--- src/Entity/Process.php
+++ src/Entity/Process.php
@@ -8,6 +8,21 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: ProcessRepository::class)]
 class Process
 {
+    public const PROCESS = [
+        1 => 'En cours',
+        2 => 'Terminé',
+    ];
+
+    public const STATUT = [
+        1 => 'Candidature envoyée',
+        2 => 'Candidature récéptionnée',
+        3 => 'Candidature en cours d\'examen',
+        4 => 'Entretien en cours',
+        5 => 'Candidat retenu pour le poste',
+        6 => 'Candidat non retenu pour le poste',
+        7 => 'Réponse reçue',
+    ];
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -21,16 +36,19 @@ class Process
 
     public function getId(): ?int
     {
-        return $this->id;
+        return $this-> id;
     }
 
     public function getProcess(): ?int
     {
-        return $this->process;
+        return $this-> process;
     }
 
     public function setProcess(int $process): static
     {
+        if (!array_key_exists($process, self::PROCESS)) {
+            return $this;
+        }
         $this->process = $process;
 
         return $this;
@@ -38,12 +56,15 @@ class Process
 
     public function getStatut(): ?int
     {
-        return $this->statut;
+        return $this-> statut;
     }
 
     public function setStatut(int $statut): static
     {
-        $this->statut = $statut;
+        if (!array_key_exists($statut, self::STATUT)) {
+            return $this;
+        }
+        $this-> statut = $statut;
 
         return $this;
     }
