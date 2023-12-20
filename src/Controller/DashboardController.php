ca52890b04095d955ac574c98f34f241c4679d94
Jessica Keller <jessica.keller68@gmail.com>
2023-12-20 10:35:42 +0100

    ajout de la vérification

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DashboardController extends AbstractController
 {
@@ -13,7 +15,9 @@ class DashboardController extends AbstractController
     {
         // Récupérer l'utilisateur connecté
         $user = $this->getUser();
-
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('Vous n\'avez pas le droit d\'accéder à cette page.');
+        }
         return $this->render('dashboard/index.html.twig', [
             'user' => $user,
         ]);
