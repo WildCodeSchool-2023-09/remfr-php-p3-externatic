33f49eeba55bb7bf07b2874ede9d10117d1c45c2
Jessica Keller <jessica.keller68@gmail.com>
2023-12-20 11:51:16 +0100

    ajout dashboard admin correction controller

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 #[Route('/admin', name: 'admin_')]
 class AdminDashboardController extends AbstractController
