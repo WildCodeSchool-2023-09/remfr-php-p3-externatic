cd30853db7fe0ed42b4bfc29842bb3c88cc95e54
Lionel D <lionel.deri@gmail.com>
2023-12-20 09:32:57 +0100

    Pages twig des cartes Offers et description des offers + Route de base

--- src/Controller/OffersController.php
+++ src/Controller/OffersController.php
@@ -1,11 +1,11 @@
 <?php
+
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class OffersController extends AbstractController
 {
