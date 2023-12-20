be85bcac7c18d022c19e627b8faf9be000ff2761
Adrien Cremeaux <adrien.cremeaux@outlook.fr>
2023-12-20 17:01:07 +0100

    S3US7_Securisation_des_routes - sécurisation de la route offer/index

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -6,6 +6,7 @@ use App\Entity\Offer;
 use App\Form\OfferType;
 use App\Repository\OfferRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,9 +15,21 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/offer', name: 'offer_')]
 class OfferController extends AbstractController
 {
+    private Security $security;
+
+    public function __construct(
+        Security $security
+    ) {
+        $this->security = $security;
+    }
+
     #[Route('/', name: 'index', methods: ['GET'])]
     public function index(OfferRepository $offerRepository): Response
     {
+        if (!($this->security->isGranted('ROLE_ADMIN'))) {
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('offer/index.html.twig', [
             'offers' => $offerRepository->findAll(),
         ]);
