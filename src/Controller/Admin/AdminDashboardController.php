db029b46456ac200d618b8d7e2cf25c01c64c8a6
Jessica Keller <jessica.keller68@gmail.com>
2023-12-20 17:34:20 +0100

    correction dashboard admin

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -14,13 +14,6 @@ class AdminDashboardController extends AbstractController
     #[Route('/', name: 'dashboard', methods:['GET'])]
     public function index(): Response
     {
-        // Vérifie si l'utilisateur est connecté et a le rôle d'administrateur (admin)
-        if ($this->getUser() && in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true)) {
-            // Si l'utilisateur est un admin, affiche le tableau de bord de l'admin
-            return $this->render('admin/dashboard.html.twig');
-        } else {
-            // Si l'utilisateur n'est pas un admin, redirection vers la page d'accueil
-            return $this->redirectToRoute('home/index.html.twig');
-        }
+        return $this->render('admin/dashboard.html.twig');
     }
 }
