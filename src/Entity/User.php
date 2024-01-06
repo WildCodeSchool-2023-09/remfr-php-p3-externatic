5f597302dbb3b810210d3500cb5e8fb6bf05e04d
Adrien Cremeaux <adrien.cremeaux@outlook.fr>
2024-01-06 19:03:00 +0100

    S3US16-dashboard - création de la page liste des candidats et ajout de la fonction permettant de trier les candidats seulement

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -196,6 +196,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function hasRole(string $role): bool
+    {
+        return in_array($role, $this->roles);
+    }
+
     public function getPhone(): ?int
     {
         return $this->phone;
