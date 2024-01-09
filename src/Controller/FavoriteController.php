8c2e979457a8ca7a07083397c6c9a9f8e78dc701
Adrien Cremeaux <adrien.cremeaux@outlook.fr>
2024-01-09 20:14:56 +0100

    S4U1_gestion_favoris - ajout front favoris

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -6,6 +6,7 @@ use App\Entity\Offer;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +15,15 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/favorites', name: 'favorites_')]
 class FavoriteController extends AbstractController
 {
+    private Security $security;
+    /** Afficher tous les utilisateurs */
+
+    public function __construct(
+        Security $security
+    ) {
+        $this->security = $security;
+    }
+
     #[Route('/add/{offer_id}', name: 'apply', methods:['GET'])]
     public function favorite(
         Request $request,
@@ -27,5 +37,19 @@ class FavoriteController extends AbstractController
         $entityManager->flush();
 
         return $this->redirectToRoute('offer_public_detail', ['id' => $offer->getId()]);
+    }
+
+    #[Route('/list', name: 'list', methods:['GET'])]
+    public function offerFavorite(
+        Request $request,
+        EntityManagerInterface $entityManager,
+    ): Response {
+        if (!($this->security->isGranted('ROLE_USER'))) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('offer_public/favorites.html.twig', [
+            'user' => $this->getUser(),
+        ]);
     }
 }
